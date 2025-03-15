5480d8f5d9f4a80eafb9c2cccb707faa0d4f5916
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:56:15 +0700

    feat: Add reactive-caller🫸🌀✏️📗🐧🐳

---
+++ reactive-caller/src/main/java/id/my/hendisantika/reactivecaller/ReactiveCallerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.reactivecaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCallerApplication.class, args);
+	}
+
+}
---
+++ reactive-caller/src/test/java/id/my/hendisantika/reactivecaller/ReactiveCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.reactivecaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
