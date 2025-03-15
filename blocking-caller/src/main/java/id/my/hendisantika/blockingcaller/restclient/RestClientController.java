9d13cbe7e8acf1b23f7415f3ecf3207da90ad8d0
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:49:11 +0700

    feat: Add RestClientController🫸🌀✏️📗🐧🐳

---
+++ blocking-caller/src/main/java/id/my/hendisantika/blockingcaller/restclient/RestClientController.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.blockingcaller.restclient;
+
+import id.my.hendisantika.blockingcaller.EchoService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-http-client-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 04.48
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/rc")
+public class RestClientController {
+
+    private final RestClient restClient;
+    private final EchoService echoService;
+
+    public RestClientController(RestClient restClient, @Qualifier("rc") EchoService echoService) {
+        this.restClient = restClient;
+        this.echoService = echoService;
+    }
+}
