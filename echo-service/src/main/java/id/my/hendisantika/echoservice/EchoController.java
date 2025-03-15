3b30f0777061f711e5f3085e67c85c1c472890e0
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:38:49 +0700

    feat: Add EchoController🫸🌀✏️📗🐧🐳

---
+++ echo-service/src/main/java/id/my/hendisantika/echoservice/EchoController.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.echoservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-http-client-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 04.38
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class EchoController {
+}
