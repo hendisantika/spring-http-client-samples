876cb86b334d2e0d07ca71264ccc4d17415718df
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:38:05 +0700

    feat: Add Echo Record🫸🌀✏️📗🐧🐳

---
+++ echo-service/src/main/java/id/my/hendisantika/echoservice/Echo.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.echoservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-http-client-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 04.37
+ * To change this template use File | Settings | File Templates.
+ */
+public record Echo(String message) {
+}
