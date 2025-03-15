6ac70f539de83c17f501200bc190c108561897f7
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:42:13 +0700

    feat: Add blocking caller🫸🌀✏️📗🐧🐳

---
+++ blocking-caller/src/main/java/id/my/hendisantika/blockingcaller/BlockingCallerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.blockingcaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingCallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockingCallerApplication.class, args);
+	}
+
+}
---
+++ blocking-caller/src/test/java/id/my/hendisantika/blockingcaller/BlockingCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.blockingcaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingCallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
