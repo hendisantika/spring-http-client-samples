f293da8ea59f59035fe40c28be57fada2f62bde3
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:39:16 +0700

    feat: Add echo in EchoController🫸🌀✏️📗🐧🐳

--- echo-service/src/main/java/id/my/hendisantika/echoservice/EchoController.java
+++ echo-service/src/main/java/id/my/hendisantika/echoservice/EchoController.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.echoservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,4 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class EchoController {
+    @GetMapping("/echo/{message}")
+    public Echo echo(@PathVariable String message) {
+        return new Echo("Echo:" + message);
+    }
 }
