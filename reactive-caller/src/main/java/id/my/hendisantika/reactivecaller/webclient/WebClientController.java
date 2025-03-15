e595386f4c12ed98ec7849f446212bcb8b2ac21f
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 05:09:48 +0700

    feat: Add WebClientController🫸🌀✏️📗🐧🐳

---
+++ reactive-caller/src/main/java/id/my/hendisantika/reactivecaller/webclient/WebClientController.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.reactivecaller.webclient;
+
+import id.my.hendisantika.reactivecaller.EchoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-http-client-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 05.09
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/wc")
+public class WebClientController {
+
+    private final WebClient webClient;
+    private final EchoService echoService;
+
+    public WebClientController(WebClient webClient, EchoService echoService) {
+        this.webClient = webClient;
+        this.echoService = echoService;
+    }
+}
