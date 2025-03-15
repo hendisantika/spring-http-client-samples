58135640e521039e1c2e45f55152820d1362a2dc
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 04:59:19 +0700

    feat: Add Echo Record🫸🌀✏️📗🐧🐳

---
+++ reactive-caller/src/main/java/id/my/hendisantika/reactivecaller/Echo.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.reactivecaller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-http-client-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 04.59
+ * To change this template use File | Settings | File Templates.
+ */
+public record Echo(String message) {
+}
